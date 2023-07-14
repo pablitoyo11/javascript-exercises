94ae061554d2785df4a91ee9d68c139e24ad2506
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-14 19:16:33 -0300

    04 done con teste

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(ArrayGiven,...RemoveThis) {
+  /*  for(let i = 0; i < ArrayGiven.length; i++){ 
+        for(let y = 0; y < RemoveThis.length; y++){
+            if ( ArrayGiven[i] === RemoveThis[y]) { 
+                ArrayGiven.splice(i, 1);
+                i--;
+            }
+        }    
+    }*/
 
+    ArrayGiven=ArrayGiven.filter(element => !RemoveThis.includes(element))
+
+return ArrayGiven;
 };
 
 // Do not edit below this line
