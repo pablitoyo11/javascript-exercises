ff9b8118583197ef041f6988195455fd9c4c30cd
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-14 16:36:50 -0300

    Done repeat string and tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(WordToRepeat,RepeatNumber) {
+    let RepeatedWordString="";
+    if (RepeatNumber<0) {
+        RepeatedWordString=("ERROR");
+        } 
+    else{
+        for (let i=0; i<RepeatNumber; i++){
+            RepeatedWordString+=WordToRepeat;
+        }
+    }
+return RepeatedWordString;
 };
 
 // Do not edit below this line
