b2b2e74da51173a3cd221e2233f7c1b38c40d555
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-16 19:12:06 -0300

    wrote cleaner code, commented the other one

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,14 @@
 const leapYears = function(YearGiven) {
-
-
-    if ((YearGiven%400==0)&&(YearGiven%100==0))
+/*
+if ((YearGiven%400==0)&&(YearGiven%100==0))
         return true;
     else
         if((YearGiven%4==0)&&!(YearGiven%400==0)&&!(YearGiven%100==0))
             return true;
-        else return false;    if((YearGiven%4==0)&&!(YearGiven%400==0)&&!(YearGiven%100==0)) return true;
-        
+        else return false;   
+*/
+       return (YearGiven%400==0)||(YearGiven%4==0)&&!(YearGiven%100==0)
+
 };
 
 // Do not edit below this line
