9077ab413a8d12eeb1b14b0605014ed19efae2fa
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-16 20:34:53 -0300

    Done 07+test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrToCel) {
+  return +((FahrToCel-32)*(5/9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelToFahr) {
+  return +((CelToFahr*(9/5))+32).toFixed(1);
 };
 
 // Do not edit below this line
