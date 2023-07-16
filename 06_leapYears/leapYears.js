5bf56a7e83690073192fd1e6f6f248534e4ea1a0
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-16 18:54:23 -0300

    done 06 +test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(YearGiven) {
 
+
+    if ((YearGiven%400==0)&&(YearGiven%100==0))
+        return true;
+    else
+        if((YearGiven%4==0)&&!(YearGiven%400==0)&&!(YearGiven%100==0))
+            return true;
+        else return false;
 };
 
 // Do not edit below this line
