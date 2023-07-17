29e46315d3b0f51dc36b35ac6071067c3d34f522
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-16 21:06:00 -0300

    done 08 calculator + test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
+const add = function(NumberToAdd,...NumbersAdd) {
+  let AddResult=NumberToAdd;
+  for (let i = 0; i < NumbersAdd.length; i++) {
+    AddResult = AddResult+NumbersAdd[i];
+  }
+  return AddResult;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(NumberToSubstract,...NumbersSubstract) {
+  let SubResult=NumberToSubstract;
+  for (let i = 0; i < NumbersSubstract.length; i++) {
+    SubResult = SubResult-NumbersSubstract[i];
+  }
+  return SubResult;
 
 };
 
-const power = function() {
+const sum = function(ArrayToSum) {
+  let AddResult=0;
+  for (let i = 0; i < ArrayToSum.length; i++) {
+    AddResult = AddResult+ArrayToSum[i];
+  }
+  return AddResult;
 	
 };
 
-const factorial = function() {
-	
+const multiply = function(NumberToMultiply,...NumbersMultiply) {
+  let AddResult=NumberToMultiply;
+  for (let i = 0; i < NumbersMultiply.length; i++) {
+    AddResult = AddResult*NumbersMultiply[i];
+  }
+  return AddResult;
+
+};
+
+const power = function(NumberToPower,PowerNumber) {
+  return (NumberToPower**PowerNumber);
+};
+
+const factorial = function(FactorialNumber) {
+	let FactorialResult=1;
+  let i=0;
+  do { i++;
+    FactorialResult*=i;
+  } while (FactorialNumber>i);
+  return FactorialResult
+
 };
 
 // Do not edit below this line
