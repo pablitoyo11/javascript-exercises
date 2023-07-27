9df072f1e8a198a166c9175da3a84aa980ed8d46
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-27 16:30:11 -0300

    DoneFibo10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(FiboPos) {
+let a,b;
+if (isNaN(FiboPos)) return NaN;
+if (FiboPos<=0) { return "OOPS";}
+if (1==FiboPos) { return 1;}
+if (2==FiboPos) { return 1;}
+
+a=fibonacci(FiboPos-1);
+b=fibonacci(FiboPos-2);
+
+return a+b;
 
 };
 
