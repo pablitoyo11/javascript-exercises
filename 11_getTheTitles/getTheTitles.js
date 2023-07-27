b6ab87df3efc748581e789740191b0323e203b61
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-27 16:55:14 -0300

    done 11 get getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookss) {
+let titlesarray=[];
+
+for (let index = 0; index < bookss.length; index++)
+{
+    titlesarray[index] = bookss[index].title;
+}
+
+return titlesarray;
 
 };
 
