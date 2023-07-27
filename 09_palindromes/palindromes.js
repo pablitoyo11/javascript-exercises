09150519b9c7cb6af769b9a1cbd4956a40220ad4
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-27 15:38:33 -0300

    palindromes 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (StringToTest) {   
+let palindrometrueorfalse=null;
 
+let StringLowerCase=StringToTest.toLowerCase();
+
+let StringLowerLetters=StringLowerCase.replace(/[^a-z0-9]/g, "");
+
+let a,b,h,i;
+
+h=StringLowerLetters.length;
+      
+        for (i=0; i<StringLowerLetters.length; i++)
+       { 
+            h--;
+            a=StringLowerLetters[i];
+            b=StringLowerLetters[h];
+            if (StringLowerLetters[i]==StringLowerLetters[h])
+                palindrometrueorfalse=true;
+            else
+            {
+                palindrometrueorfalse=false;
+                i=StringLowerLetters.length;
+            }   
+           
+        }
+    
+
+return palindrometrueorfalse;
 };
 
 // Do not edit below this line
