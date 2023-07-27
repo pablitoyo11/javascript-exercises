dcf74b1ad0290e7a59efba46a1b2501556844866
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2023-07-27 18:11:21 -0300

    done 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(ArrayDeRandoms) {
 
+
+let OldestPos=null;
+let Oldest=0;
+
+
+for (let index = 0; index < ArrayDeRandoms.length; index++)
+{
+    if (!ArrayDeRandoms[index].hasOwnProperty("yearOfDeath")) 
+    {
+        if (Oldest<2023-ArrayDeRandoms[index].yearOfBirth)
+        {
+            Oldest=2023-ArrayDeRandoms[index].yearOfBirth;
+            OldestPos=index;
+        }
+    }
+    else
+    {        
+        if (Oldest<=ArrayDeRandoms[index].yearOfDeath-ArrayDeRandoms[index].yearOfBirth)
+        {
+            Oldest=ArrayDeRandoms[index].yearOfDeath-ArrayDeRandoms[index].yearOfBirth;
+            OldestPos=index;
+        }
+    }
+}    
+
+return ArrayDeRandoms[OldestPos];
 };
 
 // Do not edit below this line
